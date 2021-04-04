9241035b97598645fc278e9ff944724577b20a87
Ewan-D <ewanduffus@gmail.com>
2021-04-04 18:54:37 +0000

    add pass through for css files

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,23 @@
-// my config for baseEleventy
+// eleventy config
 
 const pluginNavigation = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig){
-	
-	// add plugins
+
 	eleventyConfig.addPlugin(pluginNavigation);
 	
-	
+	//copy css to the output
+	eleventyConfig.addPassthroughCopy('src/css/style.css');
+
     return {
 	templateFormats: [
 		"md",
 		"njk",
 		"html",
 	],
-
-
-        dir: {
-            input: "src",
-            output: "dist",
+    dir: {
+        input: "src",
+        output: "dist",
         }
-
     };
 };
