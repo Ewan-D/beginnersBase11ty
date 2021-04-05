976ea851ddaed1b3b3a7f49abdb3b459436f7e3c
Ewan-D <ewanduffus@gmail.com>
2021-04-05 14:14:07 +0000

    tidy up and make slightly easier to read

--- .eleventy.js
+++ .eleventy.js
@@ -4,30 +4,29 @@ const pluginNavigation = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig){
 
-	eleventyConfig.addPlugin(pluginNavigation);
+    eleventyConfig.addPlugin(pluginNavigation);
 
     //luxon date formating
     eleventyConfig.addFilter("readableDate", dateObj => {
         return DateTime.fromJSDate(dateObj, {zone: 'UTC'}).toFormat("dd LL yyyy")
     })
-
-    //luxon make it accessible for html datetime="yyyy-LL-dd"
+    //and make it accessible for html standards: datetime="yyyy-LL-dd"
     eleventyConfig.addFilter("dateToString", dateObj => {
         return DateTime.fromJSDate(dateObj, {zone: 'UTC'}).toFormat("yyyy-LL-dd")
     })
 	
-	//copy css to the output
-	eleventyConfig.addPassthroughCopy('src/css/style.css');
+    //copy css to the output
+    eleventyConfig.addPassthroughCopy('src/css/style.css');
 
     return {
 	templateFormats: [
-		"md",
-		"njk",
-		"html",
+	    "md",
+	    "njk",
+	    "html",
 	],
-    dir: {
-        input: "src",
-        output: "dist",
+        dir: {
+            input: "src",
+            output: "dist",
         }
     };
 };
